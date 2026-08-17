5c46e522a9cbb54ee0ec8b6599f75b2d5c621d96
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-17 20:14:55 +0300

    refactored the review controller

--- reviewms/src/main/java/com/jobapplication/reviewms/Review/controller/ReviewController.java
+++ reviewms/src/main/java/com/jobapplication/reviewms/Review/controller/ReviewController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jobapplication.reviewms.Review.dto.ReviewRequest;
 import com.jobapplication.reviewms.Review.dto.ReviewResponse;
+import com.jobapplication.reviewms.Review.entity.ReviewEntity;
+import com.jobapplication.reviewms.Review.service.ReviewService;
 import com.jobapplication.reviewms.Review.service.Impl.ReviewServiceImpl;
 
 import java.util.List;
@@ -23,20 +26,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/companies/{companyId}")
+@RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
-    private final ReviewServiceImpl reviewService;
+    private final ReviewService reviewService;
 
-    @GetMapping("/reviews")
-    public ResponseEntity<List<ReviewResponse>> getReviews(@PathVariable Long companyId) {
+    @GetMapping
+    public ResponseEntity<List<ReviewResponse>> getReviews(@RequestParam Long companyId) {
         return ResponseEntity.ok(reviewService.getAllReviews(companyId));
     }
 
 
-    @PostMapping("/reviews")
+    @PostMapping
     public ResponseEntity<String> createReview(@RequestBody ReviewRequest reviewRequest, 
-        @PathVariable Long companyId) {
+        @RequestParam Long companyId) {
      boolean isCreated=reviewService.createReview(reviewRequest,companyId);
    if(isCreated){
     return ResponseEntity.ok("review created successfully");
@@ -44,26 +47,25 @@ public class ReviewController {
    return ResponseEntity.notFound().build();
 }
 
-    @GetMapping("reviews/{reviewId}")
-    public ResponseEntity<ReviewResponse> getReviewById(@PathVariable Long companyId,@PathVariable Long reviewId){
-    ReviewResponse review=reviewService.getReviewById(companyId,reviewId);
+    @GetMapping("/{reviewId}")
+    public ResponseEntity<ReviewEntity> getReviewById(@PathVariable Long reviewId){
+    ReviewEntity review=reviewService.getReviewById(reviewId);
     if(review != null){
    return ResponseEntity.ok(review);
       }
     return ResponseEntity.notFound().build();
 }
-   @PutMapping("/reviews/{reviewId}")
-   public ResponseEntity<String> updateReview(@PathVariable Long companyId,
-    @PathVariable Long reviewId,@RequestBody ReviewRequest reviewRequest){
-        boolean isUpdated=reviewService.updateReview(companyId,reviewId,reviewRequest);
+   @PutMapping("/{reviewId}")
+   public ResponseEntity<String> updateReview(@PathVariable Long reviewId,@RequestBody ReviewRequest reviewRequest){
+        boolean isUpdated=reviewService.updateReview(reviewId,reviewRequest);
         if(isUpdated){
             return ResponseEntity.ok("review updated Successfully");
         }
         return ResponseEntity.notFound().build();
     }
-   @DeleteMapping("/reviews/{reviewId}")
-    public ResponseEntity<String> deleteReview(@PathVariable Long companyId,@PathVariable Long reviewId){
-boolean isDeleted=reviewService.deleteReview(companyId,reviewId);
+   @DeleteMapping("/{reviewId}")
+    public ResponseEntity<String> deleteReview(@PathVariable Long reviewId){
+boolean isDeleted=reviewService.deleteReview(reviewId);
         if(isDeleted){
             return ResponseEntity.ok("review deleted Successfully");
         }
