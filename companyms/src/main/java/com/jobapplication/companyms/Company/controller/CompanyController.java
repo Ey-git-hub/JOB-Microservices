805f868eb71a94f5a0487b12e030d1b1c124bb81
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-19 10:26:33 -0700

    feat: made api of companies consistent with the others

--- companyms/src/main/java/com/jobapplication/companyms/Company/controller/CompanyController.java
+++ companyms/src/main/java/com/jobapplication/companyms/Company/controller/CompanyController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import java.util.List;
 
 @RestController
-@RequestMapping("/companies")
+@RequestMapping("api/v1/companies")
 @RequiredArgsConstructor
 public class CompanyController {
  private final CompanyServiceImpl companyServiceImpl;
