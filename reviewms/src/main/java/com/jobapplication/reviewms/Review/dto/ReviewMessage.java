da941abb4a9a9c8d308d4ffccc5cc8ec04ed6c83
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-20 06:42:50 +0300

    modification on review message dto

--- reviewms/src/main/java/com/jobapplication/reviewms/Review/dto/ReviewMessage.java
+++ reviewms/src/main/java/com/jobapplication/reviewms/Review/dto/ReviewMessage.java
@@ -1,20 +1,15 @@
 package com.jobapplication.reviewms.Review.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-// @RequiredArgsConstructor
-@NoArgsConstructor
+
 public class ReviewMessage {
     private Long id;
     private String title;
     private String description;
     private double rating;
     private Long companyId;
-    
-    
 }
