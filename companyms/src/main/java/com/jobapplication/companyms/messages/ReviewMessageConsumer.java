a5cb8bc76f30649264fc9faf15e68ea4d0124da2
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-09-03 23:13:57 +0300

    refactor: reorganize imports and add @Service annotation in ReviewMessageConsumer

--- companyms/src/main/java/com/jobapplication/companyms/messages/ReviewMessageConsumer.java
+++ companyms/src/main/java/com/jobapplication/companyms/messages/ReviewMessageConsumer.java
@@ -1,12 +1,13 @@
 package com.jobapplication.companyms.messages;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 
 import com.jobapplication.companyms.Company.dto.ReviewMessage;
 import com.jobapplication.companyms.Company.service.CompanyService;
 
 import lombok.RequiredArgsConstructor;
-
+@Service
 @RequiredArgsConstructor
 public class ReviewMessageConsumer {
     private final CompanyService companyService;
