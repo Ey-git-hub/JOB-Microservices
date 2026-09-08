d45e5687f408a4312278a949fe41c50657d8116f
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-09-08 21:12:45 +0300

    fix: adjust layout and button styling in Sign Up page

--- frontend/app/sign-up/page.tsx
+++ frontend/app/sign-up/page.tsx
@@ -10,7 +10,7 @@ export default function SignInPage(){
             <div className="w-1/2 bg-blue-500 flex items-center justify-center">
             <Image className="shadow-3xl round-md"  src="/logo.jpg" alt={"job application logo"} width={300} height={300}/>
             </div>
-            <div className="w-1/2 flex items-center justify-center">
+            <div className="w-screen flex items-center justify-center">
             <Card className="w-1/2 shadow-2xl">
                 <CardHeader>
                     <CardTitle className="font-mono">Sign Up</CardTitle>
@@ -23,9 +23,10 @@ export default function SignInPage(){
                         <Input className="mb-2" name="Email" type="email" placeholder="eyob@gmail.com" autoComplete="off"/>
                         <Label className="font-mono mb-1">Password</Label>
                         <Input className="mb-2" name="Password" type="Password" autoComplete="off" minLength={8}/>
+                        <div className="flex bg-black items-center justify-center rounded-md ">
                         <Link href="/dashboard">
-                        <Button className="font-mono" type="submit">Sign Up</Button>
-                        </Link>
+                        <Button className="flex font-mono justify-stretch" type="submit">Sign Up</Button>
+                        </Link></div>
 
                     </form>
                 </CardContent>
