2c80c4edfadcd124aec464f6bb1508549e9cf605
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-09-09 11:46:54 +0300

    feat: update AppSidebar layout and navigation links for improved user experience

--- frontend/components/app-sidebar.tsx
+++ frontend/components/app-sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Sidebar,
   SidebarContent,
@@ -8,24 +9,41 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { FishingRodIcon, Home, Network, Search } from "lucide-react";
 
 export function AppSidebar() {
   return (
-    <Sidebar className="">
+    <Sidebar className="shadow-2xl font-mono  rounded-md rounded-r-2xl overflow-hidden border-r">
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <a href="/dashboard">
-                    <span>Dashboard</span>
-                  </a>
+            <SidebarGroupLabel>Job Application</SidebarGroupLabel>
+        <SidebarGroupContent>
+            <SidebarMenu className="gap-3">
+                 
+                <SidebarMenuItem>
+                <SidebarMenuButton className="flex flex-col-1 ">
+                    <Home></Home><a href="/sign-up"><span>HOME</span></a>  
                 </SidebarMenuButton>
-              </SidebarMenuItem>
+                </SidebarMenuItem> 
+                <SidebarMenuItem>
+                <SidebarMenuButton className="flex flex-col-1 ">
+                    <Search></Search><a href="/sign-up"><span>FIND JOBS</span></a>  
+                </SidebarMenuButton>
+                </SidebarMenuItem>  
+                <SidebarMenuItem>
+                <SidebarMenuButton className="flex flex-col-1 ">
+                    <a href="/sign-up"><span>Dashboard</span></a>  
+                </SidebarMenuButton>
+                </SidebarMenuItem> 
+                <SidebarMenuItem>
+                <SidebarMenuButton className="flex flex-col-1 ">
+                    <Network></Network><a href="/sign-up"><span>MY NETWORK</span></a>  
+                </SidebarMenuButton>
+                </SidebarMenuItem> 
             </SidebarMenu>
-          </SidebarGroupContent>
+        </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
