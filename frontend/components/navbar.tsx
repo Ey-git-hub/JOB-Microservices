3ee0aee1c47335b7a9d01504d5582c693840e1c7
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-09-09 13:06:25 +0300

    fix: wrap buttons in Link components for proper navigation in Navbar

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 export default function Navbar(){
     return <div className="flex flex-row font-mono bg-white h-8 w-extend mt-3 justify-between px-3">
 <div className="flex flex-col-2 gap-4">
-    <Button><Briefcase/>Job Application</Button>
+   <Link href="/"> <Button><Briefcase/>Job Application</Button></Link>
 </div>
 <div className="flex gap-3">
-    <Button>Login</Button>
-    <Button><Link href="/sign-up">SignUp</Link></Button>
+    <Link href="/sign-in"><Button className="hover:bg-blue-300">Login</Button></Link>
+    <Link href="/sign-up"><Button>SignUp</Button></Link>
 </div>
     </div>
 }
