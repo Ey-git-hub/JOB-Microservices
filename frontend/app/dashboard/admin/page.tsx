e3bd12319186c62e43aa6593d35248577c0146cf
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-09-11 22:48:48 +0300

    style: clean up layout and spacing in Admin Dashboard

--- frontend/app/dashboard/admin/page.tsx
+++ frontend/app/dashboard/admin/page.tsx
@@ -20,19 +20,31 @@ export default async function AdminDashboard() {
                 </div>
             <div className="flex gap-3 mb-16 w-72">
               <Searchbar/>
-              <Button className="p-2 rounded-lg bg-white border border-gray-200 text-slate-400 hover:text-blue hover:border-slate-700 transition-colors relative">
+              <Button className="p-2 rounded-lg bg-white border border-gray-200 text-slate-400 hover:text-blue  transition-colors relative">
               <Bell className="w-4 h-4" />
               
             </Button>
               </div>
-             
-     
-    
-
+      
 </div>
- 
-            
-
+ <div className="flex gap-3 ">
+   <div className="p-5 bg-[#121215] border border-slate-800/80 rounded-xl w-40">
+            <span className="text-sm font-medium text-slate-400">Total companies</span>
+            <p className="text-3xl font-bold text-white tracking-tight">482</p>
+          </div>      
+          <div className="bg-[#121215] w-40 rounded-xl p-5">
+            <span className="text-sm font-medium text-slate-400">Pending Verification</span>
+            <p className="text-3xl font-bold text-white tracking-tight text-slate-400">17</p>
+            </div>   
+            <div className="bg-[#121215] p-5 rounded-xl">
+              <span className="text-slate-400 mb-5 ">Active Users</span>
+              <p className="text-white text-3xl font-bold tracking-tight">8,8787</p>
+            </div>
+            <div className="bg-[#121215] p-5 rounded-xl">
+              <span className="text-sm font-medium  text-slate-400">Open Reports</span>
+              <p className="font-bold text-3xl text-white">3</p>
+            </div>
+</div>
   </div>
 
       
