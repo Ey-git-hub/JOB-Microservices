2227cb739891c47f86812d4b6d4a40c932c97af4
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-09-11 20:28:03 +0300

    feat: restructure AdminDashboard layout to integrate Searchbar and improve UI

--- frontend/app/dashboard/admin/page.tsx
+++ frontend/app/dashboard/admin/page.tsx
@@ -1,54 +1,30 @@
+import Searchbar from "@/components/Search-bar";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuGroup, DropdownMenuLabel } from "@/components/ui/dropdown-menu";
 import { auth } from "@/lib/auth/auth";
+import { Search } from "lucide-react";
 
 export default async function AdminDashboard() {
   const session = await auth();
 
   return (
     <div className="font-mono">
-      <h1 className="text-3xl font-bold">Admin Dashboard</h1>
-      <p className="mt-4 text-slate-600">
-        Welcome, <span className="font-semibold">{session?.user?.name}</span>
-      </p> 
-     <div className="flex flex-row gap-5 mt-3 h-40 ">
-  {/* <Card className="w-73 shadow-xl" >
-    <CardHeader>
-      <Badge className="w-65 h-7  ">Add New Company</Badge>
-      <CardTitle>
-
-      </CardTitle>
-    </CardHeader>
-  </Card>
-  <Card className="w-73 shadow-xl" >
-    <CardHeader>
-      <Badge className="w-65 h-7  ">Active Companies List</Badge>
-      <CardTitle>
-
-      </CardTitle>
-    </CardHeader>
-  </Card>
-  <Card className="w-73 shadow-xl" >
-    <CardHeader>
-      <Badge className="w-65 h-8  ">Company Verification &Approval</Badge>
-      <CardTitle>
-
-      </CardTitle>
-    </CardHeader>
-  </Card>
- <Card className="w-73 shadow-xl" >
-    <CardHeader>
-      <Badge className="w-65 h-7  ">Company Manager </Badge>
-      <CardTitle>
-
-      </CardTitle>
-    </CardHeader>
-  </Card> */}
-  
-</div>
+      <div className="flex items-center gap-4">
+            {/* Search Input */}
+            <div className="relative w-72">
+              <Searchbar/>
+               <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+                <p className="mt-4 text-slate-600">
+                Welcome, <span className="font-semibold">{session?.user?.name}</span>
+                </p> 
+            </div>
+     
+    
 
 </div>
+
+  </div>
 
       
     
