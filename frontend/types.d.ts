fa1512178c4f5dbb8da6151ae61aaccdb5337dba
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-09-13 14:29:36 +0300

    feat: add accessToken property to User, Session, and JWT interfaces in types.d.ts

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -3,18 +3,21 @@
   declare module "next-auth" {
     interface User {
       roles: string[];
+      accessToken: string;
     }
 
     interface Session {
+      accessToken: string;
       user: {
         roles: string[];
       } & DefaultSession["user"];
     }
   }
-
+ 
   declare module "next-auth/jwt" {
     interface JWT extends DefaultJWT {
       roles: string[];
+      accessToken: string;
     }
   }
 
