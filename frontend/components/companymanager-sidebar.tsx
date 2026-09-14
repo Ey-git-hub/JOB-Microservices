8479474d4a6ce0c2a6b0952dcc7905f2e940aea7
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-09-14 13:30:30 +0300

    style: enhance CompanySidebar component with updated icons and improved menu labels for better navigation

--- frontend/components/companymanager-sidebar.tsx
+++ frontend/components/companymanager-sidebar.tsx
@@ -11,11 +11,11 @@ import {
 } from "@/components/ui/sidebar";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { FishingRodIcon, Folder, Home, Network, Search, UserRound } from "lucide-react";
+import { BarChart, BriefcaseBusinessIcon, Building, Building2, Calendar, FileText, FishingRodIcon, Folder, Globe2, Home, Network, Search, Send, Settings2, ShieldCheck, UserRound, Users2 } from "lucide-react";
 
 export function CompanySidebar() {
   return (
-    <Sidebar className="shadow-2xl font-mono  rounded-r-2xl overflow-hidden border-r mt-20">
+    <Sidebar className="absolute mt-20 overflow-hidden rounded-r-2xl border-r border-slate-200 bg-white font-mono text-slate-700 shadow-xl">
       <SidebarContent>
         <SidebarGroup>
             <SidebarGroupLabel>Job Application</SidebarGroupLabel>
@@ -24,27 +24,56 @@ export function CompanySidebar() {
                  
                 <SidebarMenuItem>
                 <SidebarMenuButton className="flex flex-col-1 ">
-                    <Home></Home><a href="/sign-up"><span>HOME</span></a>  
+                    <Home/><a href="/sign-up"><span>Dashboard</span></a>  
                 </SidebarMenuButton>
                 </SidebarMenuItem> 
                 <SidebarMenuItem>
                 <SidebarMenuButton className="flex flex-col-1 ">
-                    <Search></Search><a href="/sign-up"><span>FIND JOBS</span></a>  
+                    <BriefcaseBusinessIcon/><a href="/sign-up"><span>Job Postings</span></a>  
                 </SidebarMenuButton>
                 </SidebarMenuItem>  
                
                 <SidebarMenuItem>
                 <SidebarMenuButton className="flex flex-col-1 ">
-                    <Network></Network><a href="/sign-up"><span>MY NETWORK</span></a>  
+                    <FileText/><a href="/sign-up"><span>Applicants</span></a>  
                 </SidebarMenuButton>
                 </SidebarMenuItem> 
                  <SidebarMenuItem>
                 <SidebarMenuButton className="flex flex-col-1 ">
-                    <UserRound></UserRound><a href="/sign-up"><span>MY PORTFOLIO</span></a>  
+                    <Calendar/><a href="/sign-up"><span>Interviews</span></a>  
+                </SidebarMenuButton>
+                </SidebarMenuItem> 
+                <SidebarMenuItem>
+                <SidebarMenuButton className="flex flex-col-1 ">
+                    <Building2/><a href="/sign-up"><span>Company Profile</span></a>  
+                </SidebarMenuButton>
+                </SidebarMenuItem> 
+                <SidebarMenuItem>
+                <SidebarMenuButton className="flex flex-col-1 ">
+                    <BarChart/><a href="/sign-up"><span>Analytics</span></a>  
                 </SidebarMenuButton>
                 </SidebarMenuItem> 
             </SidebarMenu>
         </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            Team
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <Users2/><a href="/sign-up"><span>Team</span></a> 
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <Settings2/><a href="/sign-up"><span>Settings</span></a> 
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
