fd85ae532df2b84ca939f02739a1146ca0db4d6f
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-09-14 15:15:04 +0300

    style: comment out ReviewMessageConsumer class and its methods for future reference

--- companyms/src/main/java/com/jobapplication/companyms/messages/ReviewMessageConsumer.java
+++ companyms/src/main/java/com/jobapplication/companyms/messages/ReviewMessageConsumer.java
@@ -1,18 +1,18 @@
-package com.jobapplication.companyms.messages;
+// package com.jobapplication.companyms.messages;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Service;
+// import org.springframework.amqp.rabbit.annotation.RabbitListener;
+// import org.springframework.stereotype.Service;
 
-import com.jobapplication.companyms.Company.dto.ReviewMessage;
-import com.jobapplication.companyms.Company.service.CompanyService;
+// import com.jobapplication.companyms.Company.dto.ReviewMessage;
+// import com.jobapplication.companyms.Company.service.CompanyService;
 
-import lombok.RequiredArgsConstructor;
-@Service
-@RequiredArgsConstructor
-public class ReviewMessageConsumer {
-    private final CompanyService companyService;
-    @RabbitListener(queues = "companyReviewQueue")
-    public void consumeMessage(ReviewMessage message) {
-        companyService.updateCompanyRating(message);
-    }
-}
+// import lombok.RequiredArgsConstructor;
+// @Service
+// @RequiredArgsConstructor
+// public class ReviewMessageConsumer {
+//     private final CompanyService companyService;
+//     @RabbitListener(queues = "companyReviewQueue")
+//     public void consumeMessage(ReviewMessage message) {
+//         companyService.updateCompanyRating(message);
+//     }
+// }
